1d76170a95dd8626c51ff869f32263bf7ee3739c
kaegg <kauaneguchi@hotmail.com>
2022-06-09 16:09:59 -0300

    30MinDia-3NLW

--- DoctorCare/dia-1.js
+++ DoctorCare/dia-1.js
@@ -1,3 +1,15 @@
 function onScroll() {
-  navigation.cassList.add('scroll')
+  if (scrollY > 0) {
+    navegação.classList.add('scroll')
+  } else {
+    navegação.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
 }
