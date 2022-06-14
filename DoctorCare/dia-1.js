0a4e4f8394edbfadac7caad246d6bf94f3955405
kaegg <kauaneguchi@hotmail.com>
2022-06-14 03:21:39 -0300

    NLW Return – Mission Origin: Stage 4/5

--- DoctorCare/dia-1.js
+++ DoctorCare/dia-1.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll() //quando executar o arquivo executa o onScroll uma vez
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navegação.classList.add('scroll')
   } else {
     navegação.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 530) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -13,3 +29,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 900
+}).reveal(`
+  #home,
+  #home img, 
+  #home .infos, 
+  #services, 
+  #services header, 
+  #services .card,
+  #about,
+  #about header,
+  #about .content`)
